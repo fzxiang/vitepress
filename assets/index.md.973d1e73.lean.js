38c4ac94f490ae6b2fffc7bb65b0bc2b77b8bf28
fzxiang <38209301+fzxiang@users.noreply.github.com>
2022-05-10 01:52:36 +0000

    Deploying to gh-pages from @ fzxiang/vitepress@de159a361503b0d71ab2bffb50a8bb0156dcb633 🚀

---
+++ assets/index.md.973d1e73.lean.js
@@ -0,0 +1,24 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, b as createBaseVNode, d as createTextVNode } from "./app.f40d676c.js";
+const __pageData = '{"title":"This is a Custom Page","description":"","frontmatter":{"page":true},"relativePath":"index.md","lastUpdated":1652147533210}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "this-is-a-custom-page",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("This is a Custom Page "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#this-is-a-custom-page",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, "test deploy", -1);
+const _hoisted_3 = [
+  _hoisted_1,
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+var index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, index as default };
